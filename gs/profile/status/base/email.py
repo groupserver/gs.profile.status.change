4cb48d2404c6e1d493b9684c3d7b8394ff04faf3
Michael JasonSmith <mpj17@onlinegroups.net>
2015-04-29 17:41:45 -0700

    Adding a (broken) link to resend the verification message

--- gs/profile/status/base/email.py
+++ gs/profile/status/base/email.py
@@ -13,6 +13,11 @@
 #
 ############################################################################
 from __future__ import absolute_import, print_function, unicode_literals
+import sys
+if sys.version_info >= (3, ):  # pragma: no cover
+    from urllib.parse import quote
+else:  # Python 2
+    from urllib import quote
 from zope.cachedescriptors.property import Lazy
 from gs.profile.base import ProfileViewlet
 from gs.profile.email.base import EmailUser
@@ -27,9 +32,15 @@ class EmailViewlet(ProfileViewlet):
         return retval
 
     @Lazy
-    def changeEmailUrl(self):
-        r = '{0}{1}/emailsettings.html'
+    def profileUrl(self):
+        r = '{0}{1}'
         retval = r.format(self.siteInfo.url, self.userInfo.url)
+        return retval
+
+    @Lazy
+    def changeEmailUrl(self):
+        r = '{0}/emailsettings.html'
+        retval = r.format(self.profileUrl)
         return retval
 
     @Lazy
@@ -46,4 +57,11 @@ class EmailViewlet(ProfileViewlet):
     @Lazy
     def unverified(self):
         retval = self.emailUser.get_unverified_addresses()
+        return retval
+
+    def get_verification(self, address):
+        a = quote(address)
+        r = '{0}/emailsettings.html?form.resendVerificationAddress={1}'\
+            '&form.action.change=Change'
+        retval = r.format(self.profileUrl, a)
         return retval
