772b8c56bd3281307461104612e03f3fcfc2a2b8
Michael JasonSmith <mpj17@onlinegroups.net>
2015-06-10 15:10:00 -0700

    version

--- version.py
+++ version.py
@@ -62,7 +62,7 @@ def get_version():
         if release:
             version_string = version
         else:
-            # version_string = "%s.dev%s+%s" % (version, datestring, globalid)
+            # version_string = "%s.dev%s" % (version, datestring)
             version_string = "%s.dev%s" % (version, datestring)
 
     except (CommandError, ValueError, TypeError):
